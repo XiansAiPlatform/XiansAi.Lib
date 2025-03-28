5b1ef6a937c89f38b4d040256e34e5352fd5a3eb
CharukaR <CharukaR@99x.io>
2025-03-28 16:25:56 +0530

    Enabled Local Execution With Temporal through FLOW_SERVER_URL

--- Temporal/TemporalClientService.cs
+++ Temporal/TemporalClientService.cs
@@ -20,49 +20,53 @@ public class TemporalClientService : ITemporalClientService
     }
 
 
-     public async Task<ITemporalClient> GetClientAsyncLocal()
-    {
-        if (_client != null) return _client;
-
-
-
-        var options = new TemporalClientConnectOptions(new("localhost:7233")) // Local Temporal URL
-                {
-                    Namespace = "default", // Default local namespace
-                };
-
-                _client =  await TemporalClient.ConnectAsync(options);
-
-        return _client;
-    }
 
     public async Task<ITemporalClient> GetClientAsync()
     {
-        if (_client != null) return _client;
-
-        _client = await TemporalClient.ConnectAsync(new(PlatformConfig.FLOW_SERVER_URL ?? throw new InvalidOperationException("FLOW_SERVER_URL is not set"))
+        if (PlatformConfig.FLOW_SERVER_URL != "localhost:7233")
         {
-            Namespace = PlatformConfig.FLOW_SERVER_NAMESPACE ?? throw new InvalidOperationException("FLOW_SERVER_NAMESPACE is not set"),
-            Tls = await getTlsConfig(), 
-            LoggerFactory = LoggerFactory.Create(builder =>
-                builder.
-                    AddSimpleConsole(options => options.TimestampFormat = "[HH:mm:ss] ").
-                    SetMinimumLevel(LogLevel.Information)),
-        });
+            if (_client != null) return _client;
 
-        return _client;
+            _client = await TemporalClient.ConnectAsync(new(PlatformConfig.FLOW_SERVER_URL ?? throw new InvalidOperationException("FLOW_SERVER_URL is not set"))
+            {
+                Namespace = PlatformConfig.FLOW_SERVER_NAMESPACE ?? throw new InvalidOperationException("FLOW_SERVER_NAMESPACE is not set"),
+                Tls = await getTlsConfig(),
+                LoggerFactory = LoggerFactory.Create(builder =>
+                    builder.
+                        AddSimpleConsole(options => options.TimestampFormat = "[HH:mm:ss] ").
+                        SetMinimumLevel(LogLevel.Information)),
+            });
+
+            return _client;
+        }
+        else
+        {
+            if (_client != null) return _client;
+
+            var options = new TemporalClientConnectOptions(new(PlatformConfig.FLOW_SERVER_URL)) // Local Temporal URL
+            {
+                Namespace = "default", // Default local namespace
+            };
+
+            _client = await TemporalClient.ConnectAsync(options);
+
+            return _client;
+        }
+
     }
 
     private async Task<TlsOptions> getTlsConfig()
     {
-        if (String.IsNullOrEmpty(PlatformConfig.FLOW_SERVER_API_KEY)) {
-        return new()
+        if (String.IsNullOrEmpty(PlatformConfig.FLOW_SERVER_API_KEY))
+        {
+            return new()
             {
                 ClientCert = await File.ReadAllBytesAsync(PlatformConfig.FLOW_SERVER_CERT_PATH ?? throw new InvalidOperationException("FLOW_SERVER_CERT_PATH is not set")),
                 ClientPrivateKey = await File.ReadAllBytesAsync(PlatformConfig.FLOW_SERVER_PRIVATE_KEY_PATH ?? throw new InvalidOperationException("FLOW_SERVER_PRIVATE_KEY_PATH is not set")),
             };
         }
-        else {
+        else
+        {
             var apiKey = PlatformConfig.FLOW_SERVER_API_KEY;
             var certBase64 = apiKey.Split(':')[0];
             var cert = Convert.FromBase64String(certBase64);
