6746edbf08923b9722e09005f868acaec6a5e490
Hasith Yaggahavita <hasithy@99x.io>
2026-06-08 19:39:00 +0530

    Remove deprecated files and clean up solution structure by deleting unused projects and related code. This includes the removal of AgentContext, Constants, Globals, and various flow-related classes, as well as the associated project files and scripts. This refactor aims to streamline the codebase and improve maintainability.

--- XiansAi.Lib.Src/Flow/IChatInterceptor.cs
+++
@@ -1,9 +0,0 @@
-using XiansAi.Messaging;
-
-namespace XiansAi.Flow;
-
-public interface IChatInterceptor
-{
-    Task<MessageThread> InterceptIncomingMessageAsync(MessageThread messageThread);
-    Task<string?> InterceptOutgoingMessageAsync(MessageThread messageThread, string? response);
-}
--- XiansAi.Lib.Src/Flow/IKernelModifier.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.SemanticKernel;
-using XiansAi.Messaging;
-
-namespace XiansAi.Flow;
-
-public interface IKernelModifier
-{
-    Task<Kernel> ModifyKernelAsync(Kernel kernel, MessageThread messageThread);
-}
